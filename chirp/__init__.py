3a35fcddaab6ceccaa6b93b5ec2da4e2109ae39f
Matt Harvey <mattharvey@google.com>
2022-06-20 09:15:07 -0700

    Updates project name in copyright notice
    PiperOrigin-RevId: 456088426

--- chirp/__init__.py
+++ chirp/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright 2022 The Kākāpō Authors
+# Copyright 2022 The Chirp Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,5 +26,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Kākāpō project."""
+"""Chirp project."""
 __version__ = "0.1.0"
