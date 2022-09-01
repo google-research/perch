5925ccec511a361e30645ca132f3e17cabfd2cb4
Tom Denton <tomdenton@google.com>
2022-09-01 08:54:56 -0700

    Internal change.
    PiperOrigin-RevId: 471544066

--- chirp/data/soundscapes/build_soundscapes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-blaze run -c opt --copt=-mavx \
-  //third_party/py/chirp/data/soundscapes:download_and_prepare -- \
-  --gfs_user=kakapo --datasets=soundscapes --flume_exec_mode=IN_PROCESS
