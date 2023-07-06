ca1f7f6d8d55bb37ea974788a9e65efa4eedbfa1
Ethan Manilow <emanilow@google.com>
2023-07-06 14:48:43 -0700

    Adding audio & spectrogram summaries to separation training and adding a DSP chirp fake dataset.
    PiperOrigin-RevId: 546098116

--- chirp/tests/fake_dataset.py
+++ chirp/tests/fake_dataset.py
@@ -15,8 +15,40 @@
 
 """A fake dataset for testing."""
 
+from typing import Tuple
+
 from chirp.data import bird_taxonomy
 import numpy as np
+
+
+def window_signal(signal, win_len=2000):
+  ramp = np.linspace(0.0, 1.0, win_len // 2)
+  signal[: win_len // 2] *= ramp
+  signal[-win_len // 2 :] *= ramp[::-1]
+  return signal
+
+
+def make_dsp_chirp(n_samples, start_f, stop_f, linear=True):
+  """Creates a np.array with a sine sweep, aka a `chirp`."""
+  time = np.linspace(0.0, 1.0, n_samples)
+  space_fn = np.linspace if linear else np.geomspace
+  sweep_f = space_fn(start_f, stop_f, n_samples)
+  chirp_ = np.sin(np.pi * sweep_f * time)
+  return window_signal(chirp_)
+
+
+def make_random_dsp_chirps(total_len, gain=0.25):
+  """Makes a clip with two random sine sweeps (chirps)."""
+  n_samples = total_len // 4
+  chirp_clip = np.zeros((total_len,))
+
+  ch1_beg, ch2_beg = np.random.choice(np.arange(300, 2001, 50), size=(2,))
+  ch1_end, ch2_end = np.random.choice(np.arange(800, 3801, 50), size=(2,))
+  chirp1 = make_dsp_chirp(n_samples, ch1_beg, ch1_end, linear=False)
+  chirp2 = make_dsp_chirp(n_samples, ch2_beg, ch2_end, linear=True)
+  chirp_clip[n_samples : n_samples * 2] = chirp1
+  chirp_clip[int(n_samples * 2.5) : int(n_samples * 3.5)] = chirp2
+  return chirp_clip * gain
 
 
 class FakeDataset(bird_taxonomy.BirdTaxonomy):
@@ -28,33 +60,46 @@ class FakeDataset(bird_taxonomy.BirdTaxonomy):
         'test': self._generate_examples(20),
     }
 
+  @staticmethod
+  def _make_signal(shape: Tuple[int, ...]) -> np.ndarray:
+    return np.random.uniform(-1.0, 0.99, shape)
+
+  def _generate_one_example(self, i):
+    return {
+        'audio': self._make_signal(self.info.features['audio'].shape).astype(
+            np.float32
+        ),
+        'recording_id': i,
+        'segment_id': -1 + i,
+        'segment_start': 17,
+        'segment_end': 17 + self.info.features['audio'].shape[0],
+        'label': np.random.choice(self.info.features['label'].names, size=[3]),
+        'bg_labels': np.random.choice(
+            self.info.features['bg_labels'].names, size=[2]
+        ),
+        'filename': 'placeholder',
+        'quality_score': np.random.choice(['A', 'B', 'C', 'D', 'E', 'F']),
+        'license': '//creativecommons.org/licenses/by-nc-sa/4.0/',
+        'country': np.random.choice(['South Africa', 'Colombia', 'Namibia']),
+        'altitude': str(np.random.uniform(0, 3000)),
+        'length': np.random.choice(['1:10', '0:01']),
+        'bird_seen': np.random.choice(['yes', 'no']),
+        'latitude': str(np.random.uniform(0, 90)),
+        'longitude': str(np.random.uniform(0, 90)),
+        'playback_used': 'yes',
+        'recordist': 'N/A',
+        'remarks': 'N/A',
+        'sound_type': np.random.choice(['call', 'song']),
+    }
+
   def _generate_examples(self, num_examples):
     for i in range(num_examples):
-      yield i, {
-          'audio': np.random.uniform(
-              -1.0, 0.99, self.info.features['audio'].shape
-          ).astype(np.float32),
-          'recording_id': i,
-          'segment_id': -1 + i,
-          'segment_start': 17,
-          'segment_end': 17 + self.info.features['audio'].shape[0],
-          'label': np.random.choice(
-              self.info.features['label'].names, size=[3]
-          ),
-          'bg_labels': np.random.choice(
-              self.info.features['bg_labels'].names, size=[2]
-          ),
-          'filename': 'placeholder',
-          'quality_score': np.random.choice(['A', 'B', 'C', 'D', 'E', 'F']),
-          'license': '//creativecommons.org/licenses/by-nc-sa/4.0/',
-          'country': np.random.choice(['South Africa', 'Colombia', 'Namibia']),
-          'altitude': str(np.random.uniform(0, 3000)),
-          'length': np.random.choice(['1:10', '0:01']),
-          'bird_seen': np.random.choice(['yes', 'no']),
-          'latitude': str(np.random.uniform(0, 90)),
-          'longitude': str(np.random.uniform(0, 90)),
-          'playback_used': 'yes',
-          'recordist': 'N/A',
-          'remarks': 'N/A',
-          'sound_type': np.random.choice(['call', 'song']),
-      }
+      yield i, self._generate_one_example(i)
+
+
+class FakeChirpDataset(FakeDataset):
+  """Fake dataset with DSP chirps; useful for debugging separation."""
+
+  @staticmethod
+  def _make_signal(shape: Tuple[int, ...]) -> np.ndarray:
+    return make_random_dsp_chirps(shape[0])
--- chirp/train/separator.py
+++ chirp/train/separator.py
@@ -16,9 +16,8 @@
 """Training loop for separation models."""
 
 import functools
-import time
-from typing import Callable
-
+import io
+from typing import Callable, Dict
 
 from absl import logging
 from chirp import export_utils
@@ -34,13 +33,18 @@ from clu import metrics as clu_metrics
 from clu import periodic_actions
 import flax
 import flax.jax_utils as flax_utils
+import imageio as iio
 import jax
 from jax import numpy as jnp
 from jax import random
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
 from ml_collections import config_dict
 import numpy as np
 import optax
 import tensorflow as tf
+
 
 EVAL_LOOP_SLEEP_S = 30
 
@@ -119,6 +123,127 @@ def initialize_model(
       ),
       train_state,
   )
+
+
+def fig_image(fig):
+  """Returns an image summary from a matplotlib figure."""
+  buffer = io.BytesIO()
+  fig.savefig(buffer, format='png', bbox_inches='tight')
+  img = iio.imread(buffer.getvalue(), format='png')
+  plt.close(fig)
+  return img
+
+
+def force_numpy(arr):
+  """Ensures that arr is a numpy array."""
+  if isinstance(arr, np.ndarray):
+    return arr
+
+  if hasattr(arr, 'numpy'):
+    # Eager mode.
+    return arr.numpy()
+  else:
+    return tf.make_ndarray(arr)
+
+
+def _audio_and_spectrogram_summaries(
+    writer: metric_writers.MetricWriter,
+    step: int,
+    title: str,
+    batch: Dict[str, jnp.array],
+    separated_audio: jnp.array,
+    sample_rate: int = 16000,
+    max_outputs: int = 5,
+):
+  """Makes audio and spectrogram summaries for MixIT models."""
+
+  def _plot_spec(data, ax, title_, do_db_scaling=True):
+    if data.ndim == 2:
+      spec = data
+    else:
+      spec = np.abs(librosa.stft(np.array(data)))
+
+    if do_db_scaling:
+      spec = librosa.amplitude_to_db(spec, ref=np.max)
+
+    librosa.display.specshow(
+        spec,
+        y_axis='mel',
+        sr=sample_rate,
+        x_axis='time',
+        ax=ax,
+    )
+    ax.set(title=title_)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.label_outer()
+
+  # For waveforms, we expect:
+  # separated_audio.shape = (tpus, batch // tpus, n_src, time)
+  # batch['source_audio'].shape = (tpus, batch // tpus, n_mix, time)
+  # batch['audio'].shape = (tpus, batch // tpus, time)
+
+  mixes_of_mixes = np.reshape(batch['audio'], (-1, *batch['audio'].shape[-1:]))
+  mixes = np.reshape(
+      batch['source_audio'], (-1, *batch['source_audio'].shape[2:])
+  )
+  separated_audio = np.reshape(
+      separated_audio, (-1, *separated_audio.shape[-2:])
+  )
+
+  batch_size, n_src, *_ = separated_audio.shape
+  n_mixes = mixes.shape[1]
+  img_size = 4
+  n_rows = max(n_src, n_mixes + 1)
+
+  for i in range(min(batch_size, max_outputs)):
+    fig, axes = plt.subplots(
+        n_rows, 2, figsize=(2 * img_size, img_size * n_rows)
+    )
+
+    # Make summary for Mixture of Mixtures (MoM)
+    mom = mixes_of_mixes[i]
+    mom_title = 'MoM'
+    _plot_spec(mom, axes[0, 0], mom_title)
+    writer.write_audios(
+        step, {f'{title}/mom{i}': mom[None, :, None]}, sample_rate=sample_rate
+    )
+
+    # Make summaries for mixes
+    for m in range(n_mixes):
+      mix = mixes[i, m, ...]
+      mix_title = f'Mix {m+1}'
+      _plot_spec(mix, axes[m + 1, 0], mix_title)
+
+      writer.write_audios(
+          step,
+          {f'{title}/mix{i}_{m}': mix[None, :, None]},
+          sample_rate=sample_rate,
+      )
+
+    # Make summaries for estimated sources
+    for s in range(n_src):
+      src = separated_audio[i, s, ...]
+      src_title = f'Est. Source {s+1}'
+      _plot_spec(src, axes[s, 1], src_title)
+
+      writer.write_audios(
+          step,
+          {f'{title}/est{i}_src{s}': src[None, :, None]},
+          sample_rate=sample_rate,
+      )
+
+    # Clean up image and turn into a summary
+    for ax in axes.flatten():
+      ax.label_outer()
+      if not ax.has_data():
+        ax.set_visible(False)
+
+    plt.tight_layout()
+    img = fig_image(fig)
+    writer.write_images(step, {f'{title}/spectrograms_{i}': img[None, ...]})
 
 
 def train(
@@ -232,6 +357,7 @@ def evaluate(
     ] = optax.sigmoid_binary_cross_entropy,
     eval_sleep_s: int = EVAL_LOOP_SLEEP_S,
     eval_steps_per_checkpoint: int = -1,
+    sample_rate_hz: int = 32_000,  # TODO(emanilow): pipe through sample rates.
 ):
   """Run evaluation."""
   train_metrics = TRAIN_METRICS.copy()
@@ -260,30 +386,41 @@ def evaluate(
     mixit_neg_snr = p_log_snr_loss(
         batch['source_audio'], estimate, loss_max_snr
     )
-    return valid_metrics_collection.gather_from_model_output(
+    return model_outputs, valid_metrics_collection.gather_from_model_output(
         taxo_loss=taxo_loss,
         mixit_neg_snr=mixit_neg_snr,
         **batch,
         **output.logits(model_outputs),
     )
 
-  writer = metric_writers.create_default_writer(workdir)
+  writer = metric_writers.create_default_writer(workdir, asynchronous=False)
   reporter = periodic_actions.ReportProgress(
       num_train_steps=num_train_steps, writer=writer
   )
   for train_state in utils.checkpoint_iterator(
       train_state, model_bundle.ckpt, workdir, num_train_steps, eval_sleep_s
   ):
+    cur_train_step = int(train_state.step)
     with reporter.timed('eval'):
       valid_metrics = valid_metrics_collection.empty()
       for valid_step, batch in enumerate(valid_dataset.as_numpy_iterator()):
         batch = jax.tree_map(np.asarray, batch)
-        new_valid_metrics = get_metrics(
+        model_outputs, new_valid_metrics = get_metrics(
             batch, flax_utils.replicate(train_state)
         )
         valid_metrics = valid_metrics.merge(
             flax_utils.unreplicate(new_valid_metrics)
         )
+
+        _audio_and_spectrogram_summaries(
+            writer,
+            cur_train_step,
+            'eval',
+            batch,
+            model_outputs.separated_audio,
+            sample_rate=sample_rate_hz,
+        )
+
         if (
             eval_steps_per_checkpoint > 0
             and valid_step >= eval_steps_per_checkpoint
@@ -292,7 +429,7 @@ def evaluate(
 
       # Log validation loss
       utils.write_metrics(
-          writer, int(train_state.step), valid_metrics.compute(prefix='valid')
+          writer, cur_train_step, valid_metrics.compute(prefix='valid')
       )
     writer.flush()
 
@@ -356,6 +493,8 @@ def run(
     tf_data_service_address: str,
 ) -> None:
   """Run the experiment."""
+  valid_dataset = None
+  train_dataset = None
   if mode == 'train':
     train_dataset, dataset_info = data_utils.get_dataset(
         is_train=True,
@@ -369,6 +508,8 @@ def run(
   elif mode == 'export':
     valid_dataset = None
     dataset_info = None
+  else:
+    raise ValueError(f'Unknown run mode: "{mode}"!')
 
   if (
       dataset_info is not None
