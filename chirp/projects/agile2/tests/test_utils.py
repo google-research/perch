f64bb54bbfc8b0e9dc411c9d3fc20165f87ca34b
Tom Denton <tomdenton@google.com>
2024-07-15 13:27:08 -0700

    Classifier training data management for Agile v2.
    PiperOrigin-RevId: 652582020

--- chirp/projects/agile2/tests/test_utils.py
+++ chirp/projects/agile2/tests/test_utils.py
@@ -17,8 +17,16 @@
 
 import os
 
+from chirp.projects.hoplite import graph_utils
+from chirp.projects.hoplite import in_mem_impl
+from chirp.projects.hoplite import interface
+from chirp.projects.hoplite import sqlite_impl
+from ml_collections import config_dict
 import numpy as np
 from scipy.io import wavfile
+
+
+CLASS_LABELS = ('alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta')
 
 
 def make_wav_files(
@@ -37,3 +45,61 @@ def make_wav_files(
         wavfile.write(f, sample_rate_hz, noise)
   audio_glob = os.path.join(base_path, '*/*.wav')
   return audio_glob
+
+
+def make_db(
+    path: str,
+    db_type: str,
+    num_embeddings: int,
+    rng: np.random.Generator,
+    embedding_dim: int = 128,
+) -> interface.GraphSearchDBInterface:
+  """Create a test DB of the specified type."""
+  if db_type == 'in_mem':
+    db = in_mem_impl.InMemoryGraphSearchDB.create(
+        embedding_dim=embedding_dim,
+        max_size=5000,
+        degree_bound=256,
+    )
+  elif db_type == 'sqlite':
+    # TODO(tomdenton): use tempfile.
+    db = sqlite_impl.SQLiteGraphSearchDB.create(
+        db_path=os.path.join(path, 'db.sqlite'),
+        embedding_dim=embedding_dim,
+    )
+  else:
+    raise ValueError(f'Unknown db type: {db_type}')
+  db.setup()
+  # Insert a few embeddings...
+  graph_utils.insert_random_embeddings(db, embedding_dim, num_embeddings, rng)
+
+  config = config_dict.ConfigDict()
+  config.embedding_dim = embedding_dim
+  db.insert_metadata('db_config', config)
+  db.commit()
+  return db
+
+
+def add_random_labels(
+    db: interface.GraphSearchDBInterface,
+    rng: np.random.Generator,
+    unlabeled_prob: float = 0.5,
+    positive_label_prob: float = 0.5,
+    provenance: str = 'test',
+):
+  """Insert random labels for a subset of embeddings."""
+  for idx in db.get_embedding_ids():
+    if rng.random() < unlabeled_prob:
+      continue
+    if rng.random() < positive_label_prob:
+      label_type = interface.LabelType.POSITIVE
+    else:
+      label_type = interface.LabelType.NEGATIVE
+    label = interface.Label(
+        embedding_id=idx,
+        label=str(rng.choice(CLASS_LABELS)),
+        type=label_type,
+        provenance=provenance,
+    )
+    db.insert_label(label)
+  db.commit()
